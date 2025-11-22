f1dc83a3afa4c8dd0a8444825e9bdb2a265a601f
Ahmed@Atef <clay674302@gmail.com>
2025-11-22 11:41:51 +0200

    feat: Initialize the profile and user models and make media configrations

--- home/models.py
+++ home/models.py
@@ -1,19 +1,18 @@
 from django.db import models
+from django.conf import settings
 
 class Profile(models.Model):
-    name = models.CharField(max_length=255)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to="") # you didn't configure where to upload the media
+    image = models.ImageField(upload_to="profile/images/") # you didn't configure where to upload the media
     description = models.TextField(null=True)
     phone = models.CharField(max_length=20)
-    email = models.EmailField()
-    country = models.CharField(max_length=255)
-    state = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
     facebook_url = models.URLField()
     github_url = models.URLField()
     instagram_url = models.URLField()
     linkedin_url = models.URLField()
-    cv = models.FileField(upload_to="") # you didn't configure this also
+    cv = models.FileField(upload_to="cv_files/") # you didn't configure this also
     game_url = models.URLField()
     
     
--- portfolioServer/settings.py
+++ portfolioServer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,7 +136,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'core.User'
